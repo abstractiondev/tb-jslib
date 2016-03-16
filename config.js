fa9ce2cafcd916eeaad24a9ca5f27312d776aa77
Kalle Launiala <kalle.launiala@protonit.net>
2016-03-17 01:03:48 +0200

    Added bower-requirejs autoconfig (not used yet)

---
+++ config.js
@@ -0,0 +1,12 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    blockUI: "bower_components/blockUI/jquery.blockUI",
+    jquery: "bower_components/jquery/dist/jquery"
+  },
+  packages: [
+
+  ]
+});
